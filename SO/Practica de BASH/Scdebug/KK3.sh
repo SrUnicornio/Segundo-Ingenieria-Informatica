98ec7cfbc18e70c6176fc76d204b9eb00ceff58c
Alvaro <alvaro18012004@gmail.com>
2023-11-10 09:46:07 +0000

    mn

---
+++ SO/Practica de BASH/Scdebug/KK3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sleep 100
+
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+  echo "AYUDA"
+  exit 0
+elif [[ "$1" == "-r" || "$1" == "--rm" ]]; then
+  echo "ELIMINAR"
+  exit 0
+else
+  echo "HOLA"
+  exit 0
+fi
+
+exit 0strace: exec: Text file busy
+strace: exec: Text file busy
